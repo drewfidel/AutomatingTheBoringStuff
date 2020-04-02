fef26fd557ef386c08517cfb361f61c95b9f76eb
drewfidel <62400308+drewfidel@users.noreply.github.com>
2020-04-02 15:03:43 -0700

    dictionaries

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+myCat = {'size': 'fat', 'color': 'red'}
+
+# print(myCat['size'])
+
+# Dictionaries are unordered
+
+# for k in myCat.keys():
+#     print(k)
+#
+# for k in myCat.items():
+#     print(k)
+
+# print(myCat)for k in myCat.keys():
+#     print(k)
+
+print(myCat.get('size')) # returns fat
