151a5b241e5cf01fb491aecf9de48923a71260c6
drewfidel <62400308+drewfidel@users.noreply.github.com>
2020-04-02 19:58:00 -0700

    Try and Exception Handling
    Raise - raise your own manual exceptions
    else - will run if try code executes
    finally - will excecute code regardless, good for shutting down things like db

--- TryandExcept.py
+++ TryandExcept.py
@@ -1,21 +1,34 @@
-def div(divideBy):
-    try:
-        return 42 / divideBy
-
-    except ZeroDivisionError:
-        print("You entered a zero number")
-
-# print(div(42)
-
-print("How many pairs of shoes do you have")
+# def div(divideBy):
+#     try:
+#         return 42 / divideBy
+#
+#     except ZeroDivisionError:
+#         print("You entered a zero number")
+#
+# # print(div(42)
+#
+# print("How many pairs of shoes do you have")
+# num = input()
+#
+# try:
+#     if int(num) > 0:
+#         print("You have a good amount of shoes!")
+#     else:
+#         print("You need more shoes than that buddy")
+#
+# except ValueError:
+#     print("Please enter a number ")
+#
 num = input()
-
 try:
     if int(num) > 0:
-        print("You have a good amount of shoes!")
-    else:
-        print("You need more shoes than that buddy")
+        print("Nice!")
 
-except ValueError:
-    print("Please enter a number ")
+except Exception:
+    print("You did not enter a number more than 0")
 
+else: # If try block exceutes fine, code under else will run
+    print("You are nice boy")
+
+finally:
+    print("this will run regardless")
