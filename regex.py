c973d8485bb4489d76b566a6bc723fab41cae7da
drewfidel <62400308+drewfidel@users.noreply.github.com>
2020-04-05 19:09:38 -0700

    regex notes

---
+++ regex.py
@@ -0,0 +1,7 @@
+import re
+
+# When Matching cases certain amount of times
+# ? means 0 or 1 times
+# + means 1 or more times
+# * zero or more times
+# . matches any character except new line character 
